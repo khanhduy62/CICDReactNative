d3573bb0ed1fb537d75ed89be952b33dbae0997b
Duy Le <duyle@hmsfirst.com>
2021-01-31 13:00:43 +0700

    track event

--- App.js
+++ App.js
@@ -14,7 +14,9 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
+import {generateTestCrash} from 'appcenter-crashes';
 
 import {
   Header,
@@ -23,50 +25,22 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { trackEvent } from 'appcenter-analytics';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <Button
+          title="CLICK"
+          onPress={() => {
+            // throw new Error('This is a test javascript crash!');
+            // generateTestCrash()
+            // trackEvent('onClick', {title: 'btn-click-2'})
+            trackEvent('CLICK-EVENT');
+          }}
+        />
       </SafeAreaView>
     </>
   );
